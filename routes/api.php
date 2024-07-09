d1059b24f5209dd0e3af9852049ab296458d2f5e
MarcelSecco1 <marcelsecco1@gmail.com>
2024-07-09 19:58:48 -0300

    feat: add config to api

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/', [UserController::class, 'index']);
